9974e2fc8184dfd0f51cfb20a18ef35801709d5a
jlui17 <justinlui17@gmail.com>
2021-10-28 03:16:46 -0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./components/Header.js";
+import Header from "./components/Header.js";
+import AboutMe from "./components/AboutMe.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <main className="main-content">
+                <AboutMe></AboutMe>
+            </main>
+        </div>
+    );
 }
 
 export default App;
