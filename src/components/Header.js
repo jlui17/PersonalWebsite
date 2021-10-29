ad420277303936c8c94e1120284d56288d4f0ad2
jlui17 <justinlui17@gmail.com>
2021-10-29 00:19:46 -0700

    structure changes and tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 function Header(props) {
     return (
         <header className="header">
-            <div className="header-content">
-                <a href="#"><img src="../images/truffle.png"></img></a>
-                <nav className="header-content-nav">
-                    <a className="header-content-nav-link" href="#">About Me</a>
-                    <a className="header-content-nav-link" href="#">Experiences</a>
-                    <a className="header-content-nav-link" href="#">Projects</a>
-                    <a className="header-content-nav-link" href="#">Blogs</a>
-                    <a className="header-content-nav-link" href="#">Contact</a>
-                </nav>
+            <div className="header-align">
+                <div className="header-content">
+                    <a href="#"><img src="../images/truffle.png"></img></a>
+                    <nav className="header-content-nav">
+                        <a className="header-content-nav-link" href="#">About Me</a>
+                        <a className="header-content-nav-link" href="#">Experiences</a>
+                        <a className="header-content-nav-link" href="#">Projects</a>
+                        <a className="header-content-nav-link" href="#">Blogs</a>
+                        <a className="header-content-nav-link" href="#">Contact</a>
+                    </nav>
+                </div>
             </div>
         </header>
     );
