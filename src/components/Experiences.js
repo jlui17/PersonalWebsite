8d5ce06286adda8d87f2899be2fcb82de92e8b52
jlui17 <justinlui17@gmail.com>
2021-11-16 14:28:16 -0800

    refactored experiences and projects into functional components using state hooks

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,56 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 import jobs from "./content/jobs.json"
 
-class Experiences extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            "showMore":false
-        }
-        this.changeShow = this.changeShow.bind(this)
-    }
+function Experiences() {
+    const [showMore, setShowMore] = useState(false);
 
-    changeShow() {
-        this.setState({
-            "showMore":!this.state.showMore
-        })
-    }
-
-    render() {
-        const experiences = jobs.map((job) => {
-            return (
-                <div key={job["position"] + job["duration"]} className="experiences-content-item">
-                    <a target="_blank" href={job["link"]} rel="noopener noreferrer"><img src={"./images/"+job["image"]["file"]} alt={job["image"]["alt"]}></img></a>
-                    <div className="experiences-content-item-text">
-                        <div className="experiences-content-item-text-title">
-                            <h4>{job["position"]}</h4>
-                            <h4 style={{color:"var(--date-color)"}}>{job["duration"]}</h4>
-                        </div>
-                        <div style={{marginBottom:"var(--title-detail-spacing)"}} className="experiences-content-item-text-details">
-                            <p>{job["company"]}</p>
-                            <p>{job["location"]}</p>
-                        </div>
-                        {job["description"].map((text) => {return(<p key={text} style={{marginBottom:"var(--experiences-description-spacing)"}}>{text}</p>)})}
-                    </div>
-                </div>
-            )
-        })        
-
+    const experiences = jobs.map((job) => {
         return (
-            <section id="experiences" className="experiences">
-                <div className="experiences-align">
-                    <div className="experiences-content">
-                        <h3 style={{marginBottom:"var(--section-row-spacing)"}}>Experiences</h3>
-                        {this.state.showMore ? experiences : experiences.slice(0,3)}
-                        <div className="showmore-align">
-                            <button className="show-button" onClick={this.changeShow} style={{marginRight:"20px"}}>{this.state.showMore ? "Show Less" : "Show More"}</button>
-                            <a target="_blank" href="./justinlui_resume2021.pdf"><button className="show-button">Resume</button></a>
-                        </div>
+            <div key={job["position"] + job["duration"]} className="experiences-content-item">
+                <a target="_blank" href={job["link"]} rel="noopener noreferrer"><img src={"./images/"+job["image"]["file"]} alt={job["image"]["alt"]}></img></a>
+                <div className="experiences-content-item-text">
+                    <div className="experiences-content-item-text-title">
+                        <h4>{job["position"]}</h4>
+                        <h4 style={{color:"var(--date-color)"}}>{job["duration"]}</h4>
+                    </div>
+                    <div style={{marginBottom:"var(--title-detail-spacing)"}} className="experiences-content-item-text-details">
+                        <p>{job["company"]}</p>
+                        <p>{job["location"]}</p>
+                    </div>
+                    {job["description"].map((text) => {return(<p key={text} style={{marginBottom:"var(--experiences-description-spacing)"}}>{text}</p>)})}
+                </div>
+            </div>
+        )
+    });
+
+    return (
+        <section id="experiences" className="experiences">
+            <div className="experiences-align">
+                <div className="experiences-content">
+                    <h3 style={{marginBottom:"var(--section-row-spacing)"}}>Experiences</h3>
+                    {showMore ? experiences : experiences.slice(0,3)}
+                    <div className="showmore-align">
+                        <button className="show-button" onClick={() => {setShowMore(!showMore)}} style={{marginRight:"20px"}}>{showMore ? "Show Less" : "Show More"}</button>
+                        <a target="_blank" href="./justinlui_resume2021.pdf"><button className="show-button">Resume</button></a>
                     </div>
                 </div>
-            </section>
-        )
-    }   
+            </div>
+        </section>
+    );
 }
 
 export default Experiences;
