34bbec4cb53ee6eade474dd70431b65da72d1139
jlui17 <justinlui17@gmail.com>
2022-05-18 02:11:42 -0700

    Update intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import "./App.css";
 import "./components/Header.js";
 import Header from "./components/Header.js";
-import Intro from "./components/Intro.js"
-import AboutMe from "./components/AboutMe.js"
-import Experiences from "./components/Experiences.js"
+import Intro from "./components/Intro.js";
+import AboutMe from "./components/AboutMe.js";
+import Experiences from "./components/Experiences.js";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 
 function App() {
-    return (
-        <div>
-            <Header></Header>
-            <main className="main-content">
-                <Intro></Intro>
-                <AboutMe></AboutMe>
-                <Experiences></Experiences>
-                <Projects></Projects>
-                <Contact></Contact>
-            </main>
+  return (
+    <div className="bg-neutral-900">
+      <Header></Header>
+      <main className="main-content">
+        <div className="flex flex-col lg:flex-row justify-between lg:justify-center items-center pt-0 min-w-[50rem] h-[40rem] mt-20 mb-20">
+          <Intro />
         </div>
-    );
+        <div>
+          <AboutMe />
+        </div>
+        <div>
+          <Experiences />
+        </div>
+        <div>
+          <Projects />
+        </div>
+        <div>
+          <Contact />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
