2615ef3307dfce6b0ceecd7109495a12ce34ef97
jlui17 <justinlui17@gmail.com>
2022-05-19 23:52:45 -0700

    Updated header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import "./components/Header.js";
-import { Header } from "./components/Header.js";
+import "./components/Header/Header.js";
+import { Header } from "./components/Header/Header.js";
 import { Intro } from "./components/Intro.js";
 import AboutMe from "./components/AboutMe.js";
 import Experiences from "./components/Experiences.js";
@@ -12,13 +12,13 @@ function App() {
     <div className="flex w-full flex-col items-center justify-center bg-neutral-900">
       <header
         id="top"
-        className="flex h-24 w-full max-w-[1000px] items-center justify-center bg-neutral-800 shadow-xl lg:rounded-b-xl"
+        className="flex h-16 w-full max-w-[1023px] items-center justify-center bg-neutral-800 shadow-xl lg:h-24 lg:rounded-b-xl"
       >
         <div className="mx-4 flex w-full items-center justify-between">
           <Header />
         </div>
       </header>
-      <div className="w-[95vw] max-w-[1000px]">
+      <div className="w-[95vw] max-w-[1023px]">
         <main className="flex w-full flex-col items-center justify-center px-4">
           <div className="mt-20 mb-20 flex h-[40rem] w-auto flex-col items-center justify-center pt-0 lg:flex-row lg:justify-between">
             <Intro />
---
+++ src/components/Header/MobileDropdown.js
@@ -0,0 +1,110 @@
+import { Squash as Hamburger } from "hamburger-react";
+import { useState, useEffect, useRef, Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+
+const useClickOutside = (ref, callback) => {
+  const handleClick = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      callback();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    document.addEventListener("touchstart", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+      document.removeEventListener("touchstart", handleClick);
+    };
+  });
+};
+
+export const MobileDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menu = useRef();
+
+  useClickOutside(menu, () => setIsOpen(false));
+
+  const highlightButton = isOpen ? "bg-neutral-700 shadow-3xl" : "";
+
+  return (
+    <div ref={menu} className="lg:hidden">
+      <Menu>
+        <Menu.Button>
+          <div
+            className={
+              "rounded-xl transition-colors duration-100 " + highlightButton
+            }
+          >
+            <Hamburger
+              toggled={isOpen}
+              toggle={setIsOpen}
+              color="white"
+              size={25}
+            />
+          </div>
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items
+            className={
+              "shadow-3xl absolute right-4 flex h-1/3 w-5/12 origin-top-right flex-col items-center justify-evenly rounded-xl bg-neutral-700 md:w-1/3"
+            }
+          >
+            <Menu.Item>
+              <a
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+                className="header-link"
+                href="#aboutme"
+              >
+                About Me
+              </a>
+            </Menu.Item>
+            <Menu.Item>
+              <a
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+                className="header-link"
+                href="#experiences"
+              >
+                Experiences
+              </a>
+            </Menu.Item>
+            <Menu.Item>
+              <a
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+                className="header-link"
+                href="#projects"
+              >
+                Projects
+              </a>
+            </Menu.Item>
+            <Menu.Item>
+              <a
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+                className="header-link"
+                href="#contact"
+              >
+                Contact
+              </a>
+            </Menu.Item>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+      <nav></nav>
+    </div>
+  );
+};
